53100ad7d9046e1568528ea17dcd67f2d049703d
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2020-04-08 17:34:16 -0500

    added SDR Garage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,6 +7,7 @@ import comple from "../Image/login1.png"
 import xposure from "../Image/xposure.png"
 import stackd from "../Image/stackd.png"
 import video from "../Image/giffy.gif"
+import sdr from "../Image/SDR.jpg" 
 import ResumePDF from "../Image/KenHongResume.pdf"
 import pictureOne from "../Image/kennethHong.png"
 import {Jumbotron, Button, Carousel, Col, Row, Container, Image} from "react-bootstrap";
@@ -28,7 +29,7 @@ const Home = (props) => {
          <div className="cBox">
             <Jumbotron className="jumbotron" fluid >
                <h1 className="headerH1">Kenneth Sungi Hong</h1>
-               <p className="headerP"> Web Developer, Designer, and Creator</p>
+               <p className="headerP"> Web Developer, Designer, and Digital Creator</p>
                <Image className="homeGif" src={video} type="gif" /> 
             <br /> 
             <Col  lg={true} ><FontAwesomeIcon className="fontAwe" size="4x" icon="arrow-down"/></Col>
@@ -81,58 +82,66 @@ const Home = (props) => {
          <div className="colorRow2">
       <h1 className="skillText"> Projects & Freelance </h1>
          <Carousel className="mainCarousel">
-      <Carousel.Item className="item">
-         <img className="caroselOne" src={xposure} alt="First slide" />
-         <Carousel.Caption>
-            <h3>Xposure</h3>
-            <p> Find. Create. Enjoy. </p>
-            <p>Dive into the photo app that helps generates ideas on locations, patterns, and moods. Powered by the 
-               Unsplash API, Xposure delivers high quality photos by the click of a button. </p>
-            <Row>
-            <Col  lg={true}><a href="https://github.com/kenhong1/Project-II-Exposure" className="cIcons" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
-            <Col  lg={true}><a href="https://whispering-plateau-38685.herokuapp.com"className="cIcons" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>
-            </Row>
-         </Carousel.Caption>
-   </Carousel.Item>
-      <Carousel.Item className="item"> <img className="caroselOne" src={comple} alt="Third slide" />
-         <Carousel.Caption>
-               <h3>Complemont</h3>
-               <p>Matching Made Easy</p>
-               <p>From picking out what goes with that top to repainting your living room. Compliemont supplies you with the best 
-                  complimentory colors with one click of the button! </p>
+            <Carousel.Item className="item">
+               <img className="caroselOne" src={xposure} alt="First slide" />
+                  <Carousel.Caption>
+                     <h3>Xposure</h3>
+                        <p> Find. Create. Enjoy. </p>
+                        <p>Dive into the photo app that helps generates ideas on locations, patterns, and moods. Powered by the 
+                           Unsplash API, Xposure delivers high quality photos by the click of a button. </p>
+                           <Row>
+                              <Col  lg={true}><a href="https://github.com/kenhong1/Project-II-Exposure" className="cIcons" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
+                              <Col  lg={true}><a href="https://whispering-plateau-38685.herokuapp.com"className="cIcons" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>
+                           </Row>
+                  </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="item"> <img className="caroselOne" src={comple} alt="Third slide" />
+               <Carousel.Caption>
+                  <h3>Complemont</h3>
+                  <p>Matching Made Easy</p>
+                  <p>From picking out what goes with that top to repainting your living room. Compliemont supplies you with the best 
+                     complimentory colors with one click of the button! </p>
+                     <Row>
+                        <Col  lg={true}><a className="cIcons" href="https://github.com/orjames/mernProject" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
+                        <Col  lg={true}><a className="cIcons" href="https://orj-mern-project.herokuapp.com/" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>
+                     </Row>
+               </Carousel.Caption>
+            </Carousel.Item >
+            <Carousel.Item className="item"> 
+               <img className="caroselOne" src={mk} alt="Third slide"/>
+               <Carousel.Caption>
+                  <h3>MKX Clicker Edition </h3>
+                  <p> Test Your Might </p>
+                  <p>Do you have what it takes to protect Earth Realm from the unknown forces? 
+                     Jump into the hotseat MKX clicker edition and test your might!</p>
                <Row>
-                  <Col  lg={true}><a className="cIcons" href="https://github.com/orjames/mernProject" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
-                  <Col  lg={true}><a className="cIcons" href="https://orj-mern-project.herokuapp.com/" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>
+                  <Col  lg={true}><a className="cIcons" href="https://github.com/kenhong1/Mortal-Kombat-X-RPG" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
+                  <Col  lg={true}><a className="cIcons" href="https://kenhong1.github.io/Mortal-Kombat-X-RPG/" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>  
                </Row>
-         </Carousel.Caption>
-      </Carousel.Item >
-      <Carousel.Item className="item" > 
-         <img className="caroselOne" src={mk} alt="Third slide"/>
-         <Carousel.Caption>
-               <h3>MKX Clicker Edition </h3>
-               <p> Test Your Might </p>
-               <p>Do you have what it takes to protect Earth Realm from the unknown forces? 
-                  Jump into the hotseat MKX clicker edition and test your might!</p>
-            <Row>
-               <Col  lg={true}><a className="cIcons" href="https://github.com/kenhong1/Mortal-Kombat-X-RPG" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
-               <Col  lg={true}><a className="cIcons" href="https://kenhong1.github.io/Mortal-Kombat-X-RPG/" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>  
-            </Row>
-         </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item className="item"> 
-         <img className="caroselOne" src={stackd} alt="Third slide"/>
-         <Carousel.Caption>
-               <h3>Stackd</h3>
-               <p> Connecting You To The Latest Tech Trends</p>
-               <p>
-               </p>
-            <Row>
-               <Col  lg={true}><a className="cIcons" href="https://github.com/kenhong1/Stackd/tree/kennybranch" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
-               <Col  lg={true}><a className="cIcons" href="https://stark-basin-81992.herokuapp.com/profile/5cb9f21b856c4a0017968c37" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>  
-            </Row>
-         </Carousel.Caption>
-      </Carousel.Item>
-   </Carousel>
+               </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="item"> 
+               <img className="caroselOne" src={stackd} alt="Third slide"/>
+               <Carousel.Caption>
+                  <h3>Stackd</h3>
+                  <p> Connecting You To The Latest Tech Trends</p>
+                  <p> Grabbing data from 3 tech media sources. Stackd allows a user to stay up to date on the latest tech trends. Save, update, and share your favorite articles with your friends on Stackd.
+                  </p>
+               <Row>
+                  <Col  lg={true}><a className="cIcons" href="https://github.com/kenhong1/Stackd/tree/kennybranch" target="_blank"> <FontAwesomeIcon size="2x" icon={['fab', "github"]} /></a></Col>
+                  <Col  lg={true}><a className="cIcons" href="https://stark-basin-81992.herokuapp.com/profile/5cb9f21b856c4a0017968c37" target="_blank"> <FontAwesomeIcon size="2x" icon={['fa', "play"]} /></a></Col>  
+               </Row>
+               </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item className="item"> 
+               <img className="caroselOne" src={sdr} alt="Third slide"/>
+               <Carousel.Caption>
+               <Row>
+                  <Col  lg={true}><a className="cIcons" href="https://sdrgarage.com/" target="_blank"> <FontAwesomeIcon size="2x" color="blue" icon={['fa', "play"]} /></a></Col>  
+               </Row>
+               </Carousel.Caption>
+            </Carousel.Item>
+         </Carousel>
          </div>      
       </div>
    </div>
